635504d3e3cbe2267fbed45d12c25d5cb66423a8
KevinALopez <005.kevin.ayala@gmail.com>
2025-04-14 11:33:33 +0200

    add header + css styles + fonts

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,42 +1,37 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
+import "./header.css";
+
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle("light-mode");
+    const mode = body.classList.contains("light-mode") ? "light" : "dark";
+    localStorage.setItem("theme", mode);
+}
 
 function Header() {
     return (
-        <>
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown
-                                title="Dropdown"
-                                id="basic-nav-dropdown"
-                            >
-                                <NavDropdown.Item href="#action/3.1">
-                                    Action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">
-                                    Something
-                                </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+        <Navbar expand="lg" sticky="top" className="navbar">
+            <Container>
+                <Navbar.Brand href="#home" className="text-primary fw-bold">
+                    Kevin Ayala
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="header-navbar-nav" />
+                <Navbar.Collapse id="header-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#home">Projects</Nav.Link>
+                        <Nav.Link href="#link">Experience</Nav.Link>
+                        <Nav.Link href="#link">About</Nav.Link>
+                        <Nav.Link href="#link">Contact</Nav.Link>
+                        <Nav.Link href="#link" onClick={toggleMode}>
+                            🌓
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
